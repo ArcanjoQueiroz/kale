0bc654c56360b6315ddc05fae487b83be53ff14d
Alexandre Arcanjo de Queiroz <alexandre.queiroz@live.com>
2019-01-25 18:04:19 -0200

    Adding CSV module

---
+++ csv/src/test/java/br/com/alexandre/kale/csv/CSVTest.java
@@ -0,0 +1,36 @@
+package br.com.alexandre.kale.csv;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import java.util.Date;
+import org.junit.Before;
+import org.junit.Test;  
+
+public class CSVTest {
+
+  private CSV csv;
+
+  @Before
+  public void setUp() {
+    this.csv = new CSV();
+  }
+
+  @Test
+  public void shouldConvertNullValues() {
+    assertEquals("", this.csv.convert(null, null));
+    assertEquals("", this.csv.convert("", null));
+    assertEquals("", this.csv.convert(" ", null));
+    assertEquals("", this.csv.convert("          ", null));
+  }
+
+  @Test
+  public void shouldCoalesceRow() {
+    final Date now = new Date();
+    assertArrayEquals(new String[] { "1", "Alexandre", now.toString(), "Male", "" }, 
+        this.csv.coalesce(
+            new Object[] { 1, "Alexandre", now, "Male", null }
+            , null));
+
+    assertEquals(0, this.csv.coalesce(null, null).length);
+  }
+}
