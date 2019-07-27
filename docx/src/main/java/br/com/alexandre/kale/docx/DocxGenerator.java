72d2281fb5e4bdd06d4d2102b2d78134c1ab1940
Alexandre Arcanjo de Queiroz <alexandre.queiroz@live.com>
2019-07-27 16:48:54 -0300

    Creating kale-docx module for Java 12+

---
+++ docx/src/main/java/br/com/alexandre/kale/docx/DocxGenerator.java
@@ -0,0 +1,27 @@
+package br.com.alexandre.kale.docx;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Map;
+import org.docx4j.XmlUtils;
+import org.docx4j.openpackaging.io3.Save;
+import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
+import org.docx4j.openpackaging.parts.WordprocessingML.MainDocumentPart;
+import org.docx4j.wml.Document;
+
+public class DocxGenerator {
+
+  public void fromTemplate(final InputStream template, final Map<String, String> context,
+      final OutputStream outputStream) throws Exception {
+    final WordprocessingMLPackage wordMlPackage = WordprocessingMLPackage.load(template);
+    final MainDocumentPart documentPart = wordMlPackage.getMainDocumentPart();            
+
+    final String xml = XmlUtils.marshaltoString(documentPart.getJaxbElement());
+    final Object obj = XmlUtils.unmarshallFromTemplate(xml, context);
+
+    documentPart.setJaxbElement((Document) obj);
+
+    final Save saver = new Save(wordMlPackage);
+    saver.save(outputStream);
+  }
+}
